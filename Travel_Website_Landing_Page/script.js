853f104637c29ce5188f3fcc6f5ece6c904bb47a
Amrit Kanoi <65844887+amritk49@users.noreply.github.com>
2020-11-23 22:02:44 +0530

    Add files via upload

---
+++ Travel_Website_Landing_Page/script.js
@@ -0,0 +1,17 @@
+function toggleMenu()
+{
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+function videoToggle()
+{
+    console.log("1");
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    trailer.classList.toggle('active');
+    video.currentTime = 0;
+    video.pause();
+}
